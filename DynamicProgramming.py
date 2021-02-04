6cf0583c3ff3eb9b7e815e3c9117ed4846794cbe
Klaudia <claudiaosowska@gmail.com>
2021-02-04 15:19:57 +0100

    dynamic programming latest version

--- DynamicProgramming.py
+++ DynamicProgramming.py
@@ -2,10 +2,11 @@ import librosa
 import numpy as np
 import matplotlib.pyplot as plt
 import librosa.display
-from scipy import *
 import scipy.fftpack
 import math
 import mido
+from copy import deepcopy
+import datetime
 
 
 def freqs_2_MIDI(freqs):
@@ -18,57 +19,68 @@ def freqs_2_MIDI(freqs):
     return midi
 
 
-def match_score(q, t):
-    if q == t: 
+def match_score(q, bucket):
+
+    if q >= bucket - 2 and q <= bucket+2:
         return 2
     else:
         return -2
 
 
 def compute_align_score(inputMIDI, targetMIDI):
+    print("len")
+    print(len(inputMIDI))
+    print(len(targetMIDI))
+    
     alignScore = []
+    for i in range(len(inputMIDI)+1):
+        alignScore.append([-i])
 
-    for i in range(len(inputMIDI)):
-        alignScore.append([])
+    for i in range(1,len(targetMIDI)+1):
+        alignScore[0].append(-i)
 
-        for j in range(len(targetMIDI)):
-            
+    for i in range(1,len(inputMIDI)+1):
+
+        for j in range(1,len(targetMIDI)+1):
             alignScore[i].append(0)
+
             if i > 0:
-                alignScore[i][j] = max(alignScore[i][j], alignScore[i-1][j])
+                alignScore[i][j] = max(alignScore[i][j], alignScore[i-1][j]-1)
             if j > 0:
-                alignScore[i][j] = max(alignScore[i][j], alignScore[i][j-1])
+                alignScore[i][j] = max(alignScore[i][j], alignScore[i][j-1]-1)
             if i > 0 and j > 0:
-                alignScore[i][j] = max(alignScore[i][j], match_score(inputMIDI[i], targetMIDI[j]) + alignScore[i-1][j-1])
-        
-    return alignScore[len(inputMIDI)-1][len(targetMIDI)-1]
+                alignScore[i][j] = max(alignScore[i][j], match_score(inputMIDI[i-1], targetMIDI[j-1]) + alignScore[i-1][j-1])
+
+    return alignScore[len(inputMIDI)][len(targetMIDI)]
 
 
 def dynamic_programming(fundamental_freqs_query, target_song_name):
+
     queryMIDI = freqs_2_MIDI(fundamental_freqs_query)
-    directory = "/Users/klaudiuszek/Desktop/Licencjat/Data/" + target_song_name + ".midi"
+    directory = "/Users/klaudiuszek/Desktop/Licencjat/Data/Testy/DB/" + target_song_name
     target = mido.MidiFile(directory, clip=True)
-    targetMIDI = []
     
-    """
-     printing MIDI
-     
-    print(targetMIDI)
-    for m in queryMIDI:
-        print(m)
-    """
+    tpb = target.ticks_per_beat
+    lastTempo = target.tracks[0][0].tempo
+    buckets = [[0 for i in range(2)] for j in range(len(target.tracks[0]))] 
+    i = 0
+    oldTime = 0
+    absoluteTime = 0 
+    absoluteTimes = []
+    noteList = []
+
     for msg in target.tracks[0]:
         if not msg.is_meta:
             if msg.type == 'note_on':
-                #print(msg.note)
-                targetMIDI.append(msg.note)
+              #  print(msg)
+                noteList.append(msg.note)
 
-    score = compute_align_score(queryMIDI, targetMIDI)
-
-    print(score)
+    score = compute_align_score(queryMIDI, noteList)
+    return score
 
 
 if __name__ == "__main__":
-    song =  "/Users/klaudiuszek/Desktop/Licencjat/Data/nucenie.wav"
-    fundamental_freqs_input = [373.95120499851237, 312.97536154258165, 314.91564759439433, 335.51088065283915, 277.42922723794953, 275.5521026183549, 246.0278496831206, 304.00218210672483, 375.21935655410795]
-    dynamic_programming(fundamental_freqs_input, "wlazlKotek5s")
+    #song_name = "GdySlicznaPannaK"
+    fund = [196.0,103.83,220.0,196.0,196.0,311.13,293.66,196.0,196.0,207.65,207.65,196.0,196.0,293.66,293.66,261.63]
+    #nazwy = {"GdySlicznaPannaA", "GdySlicznaPannaK", "SzlaDzieweczkaA", "SzlaDzieweczkaK", "KawalekPodlogiA", "KawalekPodlogiK"}
+    dynamic_programming(fund, "GdySlicznaPanna1.mid")
